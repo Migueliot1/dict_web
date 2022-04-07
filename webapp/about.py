2ae1fd03a7218518d6a2d6b6e975c711d0b3f96f
Migueliot1 <migueliot@protonmail.com>
2022-04-07 21:05:38 +0300

    Add Home and About pages.

--- webapp/about.py
+++ webapp/about.py
@@ -1,8 +1,18 @@
+import justpy as jp
+
 class AboutPage:
     '''Represents About page which has a general info about the website 
     and its functionality.
     '''
 
-    def __init__(self):
+    path = '/about'
 
-        pass
+    def serve(self):
+
+        web_page = jp.QuasarPage(tailwind=True)
+
+        div = jp.Div(a=web_page, classes='bg-gray-200 h-screen')
+        jp.Div(a=div, text='This is the About page!', classes='text-4xl m2-2')
+        jp.Div(a=div, text='There is going to be About text here...', classes='text-lg')
+    
+        return web_page
--- webapp/home.py
+++ webapp/home.py
@@ -1,6 +1,16 @@
+import justpy as jp
+
 class HomePage:
     '''Represents Home page of the website.'''
+    
+    path = '/home'
 
-    def __init__(self):
+    def serve(self):
 
-        pass
+        web_page = jp.QuasarPage(tailwind=True)
+
+        div = jp.Div(a=web_page, classes='bg-gray-200 h-screen')
+        jp.Div(a=div, text='This is the Home page!', classes='text-4xl m2-2')
+        jp.Div(a=div, text='There is going to be some text here...', classes='text-lg')
+
+        return web_page
